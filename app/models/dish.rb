7345ee7543c7f92decc3ad77fcb47126f89da25a
Raghu Betina <raghu@firstdraft.com>
2018-01-10 05:01:01 +0000

    Indirect association between dishes and venues

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -8,6 +8,10 @@ class Dish < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :venues,
+             :through => :bookmarks,
+             :source => :venue
+
   has_many   :usernames,
              :through => :bookmarks,
              :source => :username
